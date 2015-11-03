aa0cdf7cfde56c6c007a47bb8f0c44618bb7a60c
Alston <and_croning@hotmail.com>
2015-11-02 23:37:15 -0500

    Examples of Attribute Selectors in JQuery

--- Attribute_Selectors/lib/scripts/script.js
+++ Attribute_Selectors/lib/scripts/script.js
@@ -1,7 +1,24 @@
-var msg = document.getElementById("message");
-
-$('document').ready(function()
+$("document").ready( function()
     {
-        msg.textContent += "JQuery is installed";
+        $("button").click(function()
+        {
+            /*
+            //few examples using some Attrubute  selectors
+            $("p[class!='paragraph1']").css("color", "lightblue");
+            $("a[href$='.com']").css("color", "lightgreen");
+            $("a[href$='.org']").css("color","red");
+            */
+
+            //$("p[class|='paragraph']").css("color","green");
+            //$("p[class^='paragraph']").css("color","green");
+
+            //only selects specifically attribute value followed by whitespace
+            //$("p[class~='paragraph']").css("color","green");
+
+            //selects all the paragraphs that have a value of "paragraph" or provided
+            $("p[class*='paragraph']").css("color","green");
+
+        }
+        );
     }
 );
