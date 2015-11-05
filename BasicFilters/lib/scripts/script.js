d0fb546231d87c63074896bb4ff59a5b89e8c557
Alston <and_croning@hotmail.com>
2015-11-04 21:42:48 -0500

    animated selector example in Jquery

--- BasicFilters/lib/scripts/script.js
+++ BasicFilters/lib/scripts/script.js
@@ -1,6 +1,19 @@
 $("document").ready( function()
     {
-        alert("jquery is installed");
+    //Example of using :animated selector
+        function animation ()
+        {
+            $("#div1").animate({width:"50%"},"slow");
+            $("#div1").animate({width: "100%"},"slow",animation);
+        }
+
+        animation();
+
+        $("button").click( function()
+            {
+                $(":animated").css("background","green");
+            }
+        );
 
     }
 );
