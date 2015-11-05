ce1fdf483f9ef8ef2520783f25d0893c4960dc4d
Alston <and_croning@hotmail.com>
2015-11-05 02:03:45 -0500

    More Basic Filter examples in Jquery

--- BasicFilters/lib/scripts/script.js
+++ BasicFilters/lib/scripts/script.js
@@ -1,5 +1,6 @@
 $("document").ready( function()
     {
+    /*
     //Example of using :animated selector
         function animation ()
         {
@@ -12,6 +13,37 @@ $("document").ready( function()
         $("button").click( function()
             {
                 $(":animated").css("background","green");
+            }
+        );
+    */
+
+        $("button").click( function()
+            {
+                //Here are some more Selector Examples
+                //$("p:eq(1)").css("color","red");//eq selector example
+                //$("p:even").css("color","green");//even selector example
+                //$("p:odd").css("color","green");//odd selector example
+                //$("p:first").css("color","green");//first selector example
+                //$("p:last").css("color","green");//last selector example
+                //$("p:gt(2)").css("color","green");//'greater than' selector example
+                //$("p:lt(3)").css("color","green");//'less than' selector example
+
+                /*Focus Selector Example
+                $("input#input1").focus();
+                $(":focus").css("background-color","green");
+                */
+
+                //not selector example
+                //$("p:not(.para)").css("background-color","green");
+
+                //root selector example
+                //$(":root").css("background-color","green");
+
+                //Header selector example
+                //$(":header").css("background-color","green");
+
+                //language selector example
+                //$("p:lang(en)").css("background-color","green");
             }
         );
 
