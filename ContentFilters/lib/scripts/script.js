4ffb50e40a95933c69b39f3aee1e16ae95969793
Alston <and_croning@hotmail.com>
2015-11-11 23:30:03 -0500

    content Filters in Jquery examples

--- ContentFilters/lib/scripts/script.js
+++ ContentFilters/lib/scripts/script.js
@@ -3,7 +3,14 @@ $("document").ready( function()
         $("button").click(function()
             {
                 //contains() selector
-                $("p:contains(1)").css("color","green");
+                //$("p:contains(1)").css("color","green");
+
+                //has() selector
+                //will look for span element inside p element if present
+                //$("p:has(span)").css("color","green");
+
+                //parent selector
+                //$("p:parent").css("color","green");
             }
         );
     }
