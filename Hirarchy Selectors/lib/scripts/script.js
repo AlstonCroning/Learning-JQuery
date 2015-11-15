5a28d64952351e5da8c6df19352284c17297e3b0
Alston <and_croning@hotmail.com>
2015-11-15 01:28:14 -0500

    preparing files for Hirarchy Selectors in JQuery

---
+++ Hirarchy Selectors/lib/scripts/script.js
@@ -0,0 +1,25 @@
+$("document").ready( function()
+    {
+        $("button").click(function()
+            {
+                //input selector
+                //$(":input").css("background-color","green");
+
+                //text selector
+                //$(":text").css("background-color","green");
+
+                //password selector
+                //$(":password").css("background-color","green");
+
+                //radio selector
+                //$(":radio").wrap("<span style='background-color:green' />");
+
+                //checkbox selector
+                //$(":checkbox").wrap("<span style='background-color:green' />");
+
+                //checked selector
+                //$(":checked").wrap("<span style='background-color:green' />");
+            }
+        );
+    }
+);
