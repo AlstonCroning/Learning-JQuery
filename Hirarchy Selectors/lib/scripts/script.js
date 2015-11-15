637742b541ae84838a7f733b601e7ff5a7292a0d
Alston <and_croning@hotmail.com>
2015-11-15 02:53:42 -0500

    Hirarchy Selectors examples in JQuery

--- Hirarchy Selectors/lib/scripts/script.js
+++ Hirarchy Selectors/lib/scripts/script.js
@@ -2,23 +2,17 @@ $("document").ready( function()
     {
         $("button").click(function()
             {
-                //input selector
-                //$(":input").css("background-color","green");
+                //child selector
+                //$("div > span").wrap("<span style='background-color:green' />");
 
-                //text selector
-                //$(":text").css("background-color","green");
+                //decendant selector
+                //$("div  span").wrap("<span style='background-color:green' />");
 
-                //password selector
-                //$(":password").css("background-color","green");
+                //adjacent selector
+                //$("div + p").css("color","green");
 
-                //radio selector
-                //$(":radio").wrap("<span style='background-color:green' />");
-
-                //checkbox selector
-                //$(":checkbox").wrap("<span style='background-color:green' />");
-
-                //checked selector
-                //$(":checked").wrap("<span style='background-color:green' />");
+                // sibling selector
+                $("div ~ p").css("color","green");
             }
         );
     }
