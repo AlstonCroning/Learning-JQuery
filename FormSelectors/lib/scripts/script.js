8b6dc0bffed494407198bfeb9030390aa4bd609e
Alston <and_croning@hotmail.com>
2015-11-15 00:15:48 -0500

    preparing files for form selectors in Jquery

---
+++ FormSelectors/lib/scripts/script.js
@@ -0,0 +1,17 @@
+$("document").ready( function()
+    {
+        $("button").click(function()
+            {
+                //contains() selector
+                //$("p:contains(1)").css("color","green");
+
+                //has() selector
+                //will look for span element inside p element if present
+                //$("p:has(span)").css("color","green");
+
+                //parent selector
+                //$("p:parent").css("color","green");
+            }
+        );
+    }
+);
